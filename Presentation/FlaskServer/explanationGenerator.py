0479d6497e718bb4378cbb9fc2ca80650f490d86
Alex Tao <talex@student.chalmers.se>
2018-04-24 20:42:17 +0200

    Property chaining

---
+++ Presentation/FlaskServer/explanationGenerator.py
@@ -0,0 +1,25 @@
+
+# class explanationGen():
+#     def _init_(self):
+#         self.test = 1
+    
+def generateRationalePatternSummary(structure=None):
+    
+    architectureComponents = "{count} {type}" 
+    
+    summaryText = "This tab describes the {pattern} and the most high level architecture related to it. "\
+                  "This pattern is structured using {sumRoles} component types. These component types "\
+                  "has {architectureComponents} implementing them.\n\n"
+        
+    relatedEntities = "Pattern component types: \n"\
+                      "{componentTypes}\n\n"\
+                      "Architectural entities: \n"\
+                      "{architecturalEntities}\n\n"
+
+    leftPanelDescription = "The panel to the left illustrates how the architectural pattern {pattern} relates to the architecture.\n\n"
+
+    return summaryText + relatedEntities + leftPanelDescription
+
+def generateRationaleChoiceSummary = ""
+
+print(generateRationaleSummary())
